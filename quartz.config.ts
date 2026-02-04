e2794c450cd3b3bb3ea417ed7314ea703bccb2d4
404NWF3 <sven30hardner@gmail.com>
2026-02-04 20:51:33 +0800

    sync

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "404nwf3.github.io/agentic-desgon-pattern-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
